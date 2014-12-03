73292a6d6f47b93c81f16f158d82e56c79eac571
Dan Sparks <dan.sparks@clever.com>
2014-12-02 23:09:42 -0800

    Update defaults for Yosemite

--- defaults.sh
+++ defaults.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Not sure which one is right.  Fuck OSX Yosemite.
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write -g ApplePressAndHoldEnabled -bool false
 
